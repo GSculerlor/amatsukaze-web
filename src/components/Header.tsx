9a5aa913d72b775216485a449e43695d16a7b2fa
Ganendra Afrasya <gsculerlor@hotmail.com>
2022-07-24 18:56:57 +0700

    Add Texas icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
+import TexasIcon from "./TexasIcon";
+
 export default function Header() {
   return (
     <div>
       <h1 className='text-on-surface text-5xl mb-6'>
         <a>Ganendra Afrasya Salsabilla</a>
       </h1>
-      <p className='text-on-surface text-base mb-2'>Android engineer at <a href='https://virgoku.id' className='font-semibold'>Virgo</a>.
-      I code stuff like Android apps and games (rhythm games most of the time). Not a big fans of web programming tho.
+      <p className='text-on-surface text-base mb-1'>associate android developer at <a href='https://virgoku.id' className='font-semibold'>Virgo</a>.
+      I code things like android apps and games sometimes. a casual rhythm game player. 
       </p>
-      <p className='text-on-surface text-base mb-8'>A total <a href='https://arknights.fandom.com/wiki/Texas' className='font-semibold'>Texas</a> fanboi.</p>
+      <div className="flex flex-row mb-6 items-center">
+      <p className='text-on-surface text-base'>based</p>
+        <div className="flex-none ml-1"><TexasIcon /></div>
+        <p className='text-on-surface text-base'> <a href='https://arknights.fandom.com/wiki/Texas' className='font-semibold'>Texas</a> fanboi.</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/TexasIcon.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function TexasIcon() {
+  return (
+    <Image src='/texas.webp' width='32' height='32' className='rounded-full' alt='Texas Image'/>
+  );
+}
